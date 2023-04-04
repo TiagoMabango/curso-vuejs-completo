4fd1af82ef9bd0b43f8e56eec4448088cbbd3383
TiagoMabango <tiagomabango@gmail.com>
2023-04-04 07:28:57 +0100

    trabalhando com compontens

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+
+    <h1>Bem Vindo ao meu primeiro projecto</h1>
+      <button @click="entrar"> entrar</button>
+    <Usuario :nome="nome"/>
+  </div>
+</template>
+
+<script>
+
+import Usuario from "./components/Usuario.vue"
+
+
+export default {  
+  name: 'App',
+  components:{
+    Usuario
+  },
+  data(){
+    return{
+      nome: 'Sujeito Programador'
+    }
+  },
+  methods:{
+    entrar(){
+      this.nome = 'Tiago Mabango'
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
---
+++ src/components/Usuario.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <div id="usuario">
+        <h2>Olá: {{nome}}</h2>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+     name: 'Usuario',
+     props: {
+        nome: [String, Number]
+     }
+}
+</script>
+
+<style>
+  h2{
+    color: red;
+  }
+</style>
